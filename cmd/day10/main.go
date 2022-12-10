488ccee0d82bb07dfcc70e7956a2cd0e8f6a08f1
rawbits <rawbits2010@gmail.com>
2022-12-10 16:03:00 +0100

    + template debug.txt + day10 part2 with visualizer

--- cmd/day10/main.go
+++ cmd/day10/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"AoC22/internal/inputhandler"
+	"AoC22/internal/outputhandler"
 	"errors"
 	"fmt"
 	"os"
@@ -19,11 +20,56 @@ func main() {
 
 	err := runCode(lines, part1Probe)
 	if err != nil && !errors.Is(err, ErrorEndOfProgram) {
-		fmt.Printf("Error while running part 1 the code: %v", err)
+		fmt.Printf("Error while running part 1 code: %v", err)
 		os.Exit(int(inputhandler.ErrorCodeProcessing))
 	}
 
-	fmt.Printf("Result - Part1: %d", part1Probe.sumSignalStrength)
+	// Part 2
+	part2ProbeCycles := []int{40, 80, 120, 160, 200, 240}
+	part2Probe := NewDisplaySignalProbe(part2ProbeCycles)
+
+	err = runCode(lines, part2Probe)
+	if err != nil && !errors.Is(err, ErrorEndOfProgram) {
+		fmt.Printf("Error while running part 2 code: %v", err)
+		os.Exit(int(inputhandler.ErrorCodeProcessing))
+	}
+
+	fmt.Printf("Result - Part1: %d, Part 2:\n", part1Probe.SumSignalStrength)
+	vizualizeDisplaySignalProbe(part2Probe)
+}
+
+func vizualizeDisplaySignalProbe(probe *DisplaySignalProbe) {
+
+	outputhandler.Initialize()
+	defer outputhandler.Reset()
+
+	litPixelColor := outputhandler.GetColor(outputhandler.White, outputhandler.BrightGreen)
+	unlitPixelColor := outputhandler.GetForeground(outputhandler.Gray)
+
+	for _, row := range probe.Display {
+
+		var colorized string = ""
+		var currColor string = outputhandler.GetReset()
+		var lastRune rune = '\000'
+
+		for _, currRune := range row {
+
+			if currRune != lastRune {
+				lastRune = currRune
+				switch currRune {
+				case '.':
+					currColor = unlitPixelColor
+				case '#':
+					currColor = litPixelColor
+				}
+				colorized += currColor
+			}
+			colorized += string(currRune)
+		}
+
+		fmt.Println(colorized + outputhandler.GetReset())
+	}
+
 }
 
 func runCode(program []string, probe SignalProber) error {
@@ -31,18 +77,21 @@ func runCode(program []string, probe SignalProber) error {
 	// build the PC :) - here, for simplicity
 	databus := NewDataBus(program)
 	cpu := NewCPU(databus)
+	gpu := NewGPU(cpu)
 
-	probe.attachProbe(cpu)
+	probe.AttachProbe(cpu, gpu)
 
 	var currCycle int
 	for {
 		currCycle++
 
-		if probe.needsProbing(currCycle) {
-			probe.probe(currCycle)
+		gpu.Tick() // just like OpenGL, the show must go on :)
+
+		if probe.NeedsProbing(currCycle) {
+			probe.Probe(currCycle)
 		}
 
-		if err := cpu.tick(); err != nil {
+		if err := cpu.Tick(); err != nil {
 			return fmt.Errorf("CPU exception: %w", err)
 		}
 
@@ -51,11 +100,29 @@ func runCode(program []string, probe SignalProber) error {
 
 //-----------------------------------------------------------------------------
 
+// DisplaySignalProbe is the probe for Part 2
+type DisplaySignalProbe struct {
+	SignalProbe
+
+	Display []string
+}
+
+func NewDisplaySignalProbe(probingCycles []int) *DisplaySignalProbe {
+	return &DisplaySignalProbe{
+		SignalProbe: *NewSignalProbe(probingCycles),
+		Display:     make([]string, 0, len(probingCycles)),
+	}
+}
+
+func (dp *DisplaySignalProbe) Probe(cycle int) {
+	dp.Display = append(dp.Display, string(dp.gpu.currScanline[:]))
+}
+
 // SignalStrengthProbe is the probe for Part 1
 type SignalStrengthProbe struct {
 	SignalProbe
 
-	sumSignalStrength int
+	SumSignalStrength int
 }
 
 func NewSignalStrengthProbe(probingCycles []int) *SignalStrengthProbe {
@@ -64,20 +131,21 @@ func NewSignalStrengthProbe(probingCycles []int) *SignalStrengthProbe {
 	}
 }
 
-func (ssp *SignalStrengthProbe) probe(cycle int) {
-	ssp.sumSignalStrength += cycle * ssp.cpu.RegX
+func (ssp *SignalStrengthProbe) Probe(cycle int) {
+	ssp.SumSignalStrength += cycle * ssp.cpu.RegX
 }
 
 // SignalProber is the interface for a probe
 type SignalProber interface {
-	attachProbe(cpu *CPU)
-	needsProbing(cycle int) bool
-	probe(cycle int)
+	AttachProbe(cpu *CPU, gpu *GPU)
+	NeedsProbing(cycle int) bool
+	Probe(cycle int)
 }
 
 // SignalProbe is a base for an actual signal probing function
 type SignalProbe struct {
 	cpu *CPU
+	gpu *GPU
 
 	probingCycles []int
 }
@@ -88,11 +156,12 @@ func NewSignalProbe(probingCycles []int) *SignalProbe {
 	}
 }
 
-func (sp *SignalProbe) attachProbe(cpu *CPU) {
+func (sp *SignalProbe) AttachProbe(cpu *CPU, gpu *GPU) {
 	sp.cpu = cpu
+	sp.gpu = gpu
 }
 
-func (sp *SignalProbe) needsProbing(cycle int) bool {
+func (sp *SignalProbe) NeedsProbing(cycle int) bool {
 	for _, neededCycle := range sp.probingCycles {
 		if cycle == neededCycle {
 			return true
@@ -111,7 +180,7 @@ func NewDataBus(lines []string) *DataBus {
 	return &DataBus{code: lines}
 }
 
-func (db *DataBus) requestInstruction(index int) (string, error) {
+func (db *DataBus) RequestInstruction(index int) (string, error) {
 	if index > len(db.code) || index < 0 {
 		return "", fmt.Errorf("out of range")
 	} else if index == len(db.code) { // for simplicity
@@ -143,13 +212,13 @@ func NewCPU(databus *DataBus) *CPU {
 	}
 }
 
-func (cpu *CPU) tick() error {
+func (cpu *CPU) Tick() error {
 
 	// setup next instruction
 	if cpu.InstCyclesLeft <= 0 {
 		cpu.ProgramCounter++
 
-		line, err := cpu.databus.requestInstruction(cpu.ProgramCounter)
+		line, err := cpu.databus.RequestInstruction(cpu.ProgramCounter)
 		if err != nil {
 			return fmt.Errorf("error requesting instruction: %w", err)
 		}
@@ -177,17 +246,12 @@ func (cpu *CPU) tick() error {
 		}
 
 	}
-	/*
-		if currCycle <= 20 {
-			fmt.Printf("cycle: %d, regX: %d - inst: %s, cyclesleft: %d\n", currCycle, regX, lines[programCounter], instCyclesLeft)
-		}
-	*/
+
 	// process instruction
 	cpu.InstCyclesLeft--
 	switch cpu.Instruction {
 	case "noop":
 		// chill
-		//fmt.Printf("inst: noop, cyclesleft: %d|n", instCyclesLeft)
 
 	case "addx":
 		if cpu.InstCyclesLeft == 0 {
@@ -195,11 +259,49 @@ func (cpu *CPU) tick() error {
 			if err != nil {
 				return fmt.Errorf("invalid argument for addx '%s' in line number '%d'", cpu.InstArgs[0], cpu.ProgramCounter+1)
 			}
-			//fmt.Printf("inst: addx, arg: %d, cyclesleft: %d\n", val, instCyclesLeft)
 
 			cpu.RegX += val
 		}
 	}
 
 	return nil
+}
+
+//-----------------------------------------------------------------------------
+
+const scanlineLength int = 40
+const spriteLength int = 3
+
+type GPU struct {
+	currScanline [scanlineLength]byte
+	currPixelIdx int
+
+	cpu *CPU
+}
+
+func NewGPU(cpu *CPU) *GPU {
+	return &GPU{
+		currPixelIdx: -1,
+		cpu:          cpu,
+	}
+}
+
+func (gpu *GPU) Tick() {
+	gpu.currPixelIdx++
+
+	// new scanline
+	if gpu.currPixelIdx == scanlineLength {
+		for idx := range gpu.currScanline {
+			gpu.currScanline[idx] = ' '
+		}
+		gpu.currPixelIdx = 0
+	}
+
+	// register X is the middle of the sprite
+	var spriteLeftPos = gpu.cpu.RegX - spriteLength/2
+	if gpu.currPixelIdx >= spriteLeftPos && gpu.currPixelIdx < spriteLeftPos+spriteLength {
+		gpu.currScanline[gpu.currPixelIdx] = '#'
+	} else {
+		gpu.currScanline[gpu.currPixelIdx] = '.'
+	}
 }
